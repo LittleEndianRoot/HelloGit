4f5f70ea1cc9c0bf0bc380f73001ecfc7db80d06
LittleEndianRoot <littleendianroot@gmail.com>
2019-01-15 06:23:54 +0000

    Now there are a total of three branches, this one is the testing branch

--- HelloGit.c
+++ HelloGit.c
@@ -5,5 +5,6 @@ int main()
 	printf("Hello GitHub!\n");
 	printf("Testing some of the cool git features\n");
 	printf("Split from to and experimental branch for testing.\n");
+	printf("Split again, now there are a total of 3 branches. this one is testing.\n");
 	return 0;
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ This is my first push to my GitHub profile.
 
 I've switched to an experimental branch, which I will push to github.
 This is for myself so I can learn how branches appear on my github profile.
+
+Here is an example of more branching. 
+
